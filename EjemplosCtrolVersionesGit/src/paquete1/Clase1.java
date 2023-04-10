8ccc6ae022c910ef8cc5f4c42b27745005bff89a
serac <serac@MSI-Sergio>
2023-04-10 19:40:49 +0200

    xD

---
+++ EjemplosCtrolVersionesGit/src/paquete1/Clase1.java
@@ -0,0 +1,17 @@
+package paquete1;
+
+import java.util.Scanner;
+
+public class Clase1 {
+
+	public static void main(String[] args) {
+		Scanner entradan = new Scanner(System.in);
+		boolean xd = false;
+		while(xd== false) {
+			if (entradan.next().charAt(0)== 'q') xd = true;
+			System.out.println("Hola mundo");
+		}
+
+	}
+
+}
