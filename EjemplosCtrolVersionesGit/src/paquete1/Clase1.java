e95d20e40e0e819337e20114ea9bcefda923a305
serac <serac@MSI-Sergio>
2023-04-10 19:43:29 +0200

    xD

--- EjemplosCtrolVersionesGit/src/paquete1/Clase1.java
+++ EjemplosCtrolVersionesGit/src/paquete1/Clase1.java
@@ -7,9 +7,11 @@ public class Clase1 {
 	public static void main(String[] args) {
 		Scanner entradan = new Scanner(System.in);
 		boolean xd = false;
+		int c = 0;
 		while(xd== false) {
 			if (entradan.next().charAt(0)== 'q') xd = true;
-			System.out.println("Hola mundo");
+			System.out.println("Hola mundo "+c);
+			c++;
 		}
 
 	}
